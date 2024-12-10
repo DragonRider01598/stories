0f59108ef9e33700fd847d1996125446ba9438ac
DragonRider01598 <dragonrider01598@gmail.com>
2024-12-10 17:29:26 +0530

    cors apply to all doesn't work since cookie and auth is included so confuiguing

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,17 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
-//    cors({
-//    origin: process.env.FRONTEND_URL,
-//    credentials: true
-// }))
+app.use(cors({
+   origin: process.env.FRONTEND_URL,
+   credentials: true,
+   allowedHeaders: ['Content-Type', 'Authorization'],
+   credentials: true,
+}))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/story', storyRoutes);
 
-app.use('/', (req, res) => { return res.status(200).send('Server is online')})
+app.use('/', (req, res) => { return res.status(200).send('Server is online') })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
